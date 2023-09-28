c9404c4cb214cb264da5d0bdfe5879eed16411fe
Pieter Wiesner <39088471+Spartanforever1@users.noreply.github.com>
2023-09-28 17:01:44 +0200

    Requested files to be added for lucy product queries with authorization

---
+++ samples/get_products_from_lucy.py
@@ -0,0 +1,14 @@
+from dotenv import load_dotenv
+from daaily.credentials_sally import Credentials
+from daaily.transport.urllib3_http import AuthorizedHttp
+
+load_dotenv()
+
+creds = Credentials()
+s = AuthorizedHttp(creds)
+
+
+def main():
+    pass
+    r = s.request("GET", "https://lucy.daaily.com/api/v2/products")
+    print(r.data)
