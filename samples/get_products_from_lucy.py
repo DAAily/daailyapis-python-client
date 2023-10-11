d10aa1b6191776809aed583ff39b483d1fd18e4c
Pieter Wiesner <39088471+Spartanforever1@users.noreply.github.com>
2023-10-11 10:34:07 +0200

    performed additional formatting to resolve the format issues

--- samples/get_products_from_lucy.py
+++ samples/get_products_from_lucy.py
@@ -3,10 +3,11 @@ from dotenv import load_dotenv
 from daaily.credentials_sally import Credentials
 from daaily.transport.urllib3_http import AuthorizedHttp
 
-load_dotenv()
-creds = Credentials()
-s = AuthorizedHttp(creds)
 
 def main():
+    load_dotenv()
+    creds = Credentials()
+    s = AuthorizedHttp(creds)
+
     r = s.request("GET", "https://lucy.daaily.com/api/v2/products")
     print(r.data)
