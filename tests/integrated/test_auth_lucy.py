540ee7bb1c059d57797fc08aaf4098df897a1a73
Sylver11 <connectmaeuse@gmail.com>
2023-10-24 17:08:56 +0200

    od-3985 moving integrated test client intantiation into the function calls

--- tests/integrated/test_auth_lucy.py
+++ tests/integrated/test_auth_lucy.py
@@ -7,13 +7,12 @@ MANUFACTURER_ID = 1
 MANUFACTURER_NAME = "This never would never exists on Architonic"
 URL = "https://lucy.daaily.com/api/v2/manufacturers"
 
-creds = Credentials()
-auth_s = daaily.transport.urllib3_http.AuthorizedHttp(creds)
-
 
 class TestAuthorizedHttpClient:
     @pytest.mark.skip("This is a integrated test and should be run manually only")
     def test_get_manufacturer_by_id(self):
+        creds = Credentials()
+        auth_s = daaily.transport.urllib3_http.AuthorizedHttp(creds)
         """Retrieve the manufacturer name from the Lucy API"""
         full_url = f"{URL}/{MANUFACTURER_ID}"
         r = auth_s.request("GET", full_url)
@@ -21,6 +20,8 @@ class TestAuthorizedHttpClient:
 
     @pytest.mark.skip("This is a integrated test and should be run manually only")
     def test_get_manufacturers_by_name_like(self):
+        creds = Credentials()
+        auth_s = daaily.transport.urllib3_http.AuthorizedHttp(creds)
         """Retrieve the manufacturer name from the Lucy API"""
         full_url = f"{URL}?manufacturer_name={MANUFACTURER_NAME}"
         r = auth_s.request("GET", full_url)
