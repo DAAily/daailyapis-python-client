5a6bfe371577fe4b37798f45f9773eb6ea6c16d6
Sylver11 <connectmaeuse@gmail.com>
2024-12-05 09:03:42 +0200

    sprin-2687 adding headers to response of franklin

--- daaily/franklin/responses.py
+++ daaily/franklin/responses.py
@@ -4,11 +4,13 @@ from dataclasses import dataclass, field
 @dataclass
 class PredictGroupResponse:
     status: str = field()
+    headers: dict = field()
     data: dict = field()
 
     @classmethod
     def from_response(cls, response):
         return cls(
             status=response.status,
+            headers=response.headers,
             data=response.data,
         )
