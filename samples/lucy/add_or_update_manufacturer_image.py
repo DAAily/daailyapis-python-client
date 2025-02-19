e4f50d1d4c3b64bada9e5a943e5e39a9f115ed0c
Sylver11 <connectmaeuse@gmail.com>
2025-02-19 10:07:35 +0200

    sprin-3443 adding func to read filename from url instead of only content disposition

--- samples/lucy/add_or_update_manufacturer_image.py
+++ samples/lucy/add_or_update_manufacturer_image.py
@@ -14,11 +14,16 @@ script_dir = os.path.dirname(os.path.abspath(__file__))
 # Construct the path to the file in the neighboring directory
 image_path = os.path.join(script_dir, "..", "assets", "vitra.jpeg")
 
+image_path = None
+
+image_url = "https://i0.wp.com/ideolux.it/wp-content/uploads/2021/03/Ideolux_Logo_White.png?fit=1584%2C254&ssl=1"
+
 # Test with image_url if image_path is None
 updated_manufacturer = client.manufacturers.add_or_update_image(
     manufacturer_id=manufacturer_id,
-    image_type="header",
+    image_type="logo",
     image_path=image_path,
+    image_url=image_url,
     description="Header Image of Manufacturer",
     direct_link={
         "url": "https://www.vitra.com/en-us/family/alcove-plume-contract",
