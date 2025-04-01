cc30cdc62ac6e9198f9ad09388f2f4f702aa4134
Sylver11 <connectmaeuse@gmail.com>
2025-04-01 12:41:40 +0200

    sprin-3848 fixing attribute example that was not working anymore

--- samples/lucy/add_or_update_product_attributes.py
+++ samples/lucy/add_or_update_product_attributes.py
@@ -3,7 +3,7 @@ import logging
 from daaily.lucy import Client
 from daaily.lucy.constants import LUCY_V2_BASE_URL_STAGING
 from daaily.lucy.enums import Service
-from daaily.lucy.resources.attribute.type import AttributeType
+from daaily.lucy.resources.attribute.type import AttributeType, AttributeValueUnit
 from daaily.lucy.utils import setup_logging
 
 # Setup logging
@@ -13,21 +13,26 @@ setup_logging(level=logging.INFO)
 client = Client(base_url=LUCY_V2_BASE_URL_STAGING)
 
 attributes = [
-    ("Cable length", "3 meters", AttributeType.FEATURE),
-    ("Lighting installation orientation", "yes", AttributeType.FEATURE),
+    ("Cable length", 300, AttributeType.FEATURE, AttributeValueUnit.CM),
+    ("Lighting installation orientation", "yes", AttributeType.FEATURE, None),
     (
         "3 star base",
         None,
         AttributeType.BASE,
+        None,
     ),  # None value will be understood as being a boolean value for True
-    ("Non existing attribute", None, AttributeType.FEATURE),
+    ("Non existing attribute", None, AttributeType.FEATURE, None),
 ]
 
 # Define the product ID
 product_id = 20321273
 
 updated_product = client.products.add_or_update_attributes(
-    product_id, attributes=attributes, service=Service.HOOVER, dry_run=True
+    product_id,
+    attributes=attributes,
+    service=Service.HOOVER,
+    overwrite_existing=False,
+    dry_run=True,
 )
 
 print(updated_product)
